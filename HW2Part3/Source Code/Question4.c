2a8b9b0cf5f160f0b24e89952e6dc373a90e982a
Justin Shearson <justinshearson@gmail.com>
2019-10-24 23:49:25 -0400

    Updated question 4

--- HW2Part3/Source Code/Question4.c
+++ HW2Part3/Source Code/Question4.c
@@ -2,16 +2,30 @@
 #include <stdlib.h>
 
 int proc1(int* inputArray, int y, int x) {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
+	int a = 0; //var_8
+	int b = 0; //var_c
+	int c = 0; //var_10
+	int d = 0; //var_4
 
 	while (d < y) {
-
+		a = 1;
+		while(a < x){
+			while(inputArray[b] == 0){
+				b = (b+1)%y;
+			}
+			a++;
+			b = (b+1)%y;
+		}
+		while(inputArray[b] == 0){
+			b = (b+1)%y;
+		}
+		
+		c = inputArray[b];
+		inputArray[b] = 0;
 		d++;
 	}
-	return d;
+
+	return b;
 }
 
 int main(void)
@@ -21,11 +35,11 @@ int main(void)
 	int z = 0;
 	int array[101];
 	while (z < y) {
-		array[z] = z;
-		z++;
+		array[z] = ++z;
 	}
 
 	printf("%d", proc1(array, y, x));
 	
 	return 0;
 }
+
